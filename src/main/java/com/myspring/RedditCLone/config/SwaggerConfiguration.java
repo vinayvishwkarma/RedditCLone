82ee6ef32d995c736e3415157b8210a734bd9133
Vinay Vishwkarma <45309427+vinayvishwkarma@users.noreply.github.com>
2020-09-10 22:27:03 +0300

    Update SwaggerConfiguration.java

--- src/main/java/com/myspring/RedditCLone/config/SwaggerConfiguration.java
+++ src/main/java/com/myspring/RedditCLone/config/SwaggerConfiguration.java
@@ -29,7 +29,7 @@ public class SwaggerConfiguration {
                 .title("Reddit Clone API")
                 .version("1.0")
                 .description("API for Reddit Clone Application")
-                .contact(new Contact("Sai Upadhyayula", "http://programmingtechie.com", "xyz@email.com"))
+                .contact(new Contact("Vinay Vishwkarma", "https://www.linkedin.com/in/vinay-vishwkarma-a923b5122/", "vinay.vishwakrma@gmail.com"))
                 .license("Apache License Version 2.0")
                 .build();
     }
